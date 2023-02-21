7d6d3ad07e755066ed9adbbc66298a4606e60703
Zene <oinkymail3@gmail.com>
2023-02-21 16:22:29 +0000

    |||||||||||||||||||||||||||||||||||||||||||||||

--- GUITest/TestElement.cs
+++ GUITest/TestElement.cs
@@ -13,55 +13,51 @@ namespace GUITest
             : base(new FixedLayout(0d, 0d, 400d, 250d))
         {
             _g = new Renderer(this);
-            _sizeAnimation = new AnimatorData(SetSize, 0.02, 0d, 1d);
+            _sizeAnimation = new AnimatorData<Vector2>(SetSize, 0.03, (400d, 250d), (410d, 260d));
         }
 
         private readonly Renderer _g;
         public override GraphicsManager Graphics => _g;
 
-        private readonly AnimatorData _sizeAnimation;
-        private void SetSize(double v)
-        {
-            Layout.Size = new Vector2(400d, 250d).Lerp((410d, 260d), v);
-        }
+        private readonly AnimatorData<Vector2> _sizeAnimation;
+        private void SetSize(Vector2 v) => Layout.Size = v;
 
         private Colour _c;
         protected override void OnUpdate(EventArgs e)
         {
-            base.OnUpdate(e);
-
             _c = new Colour(166, 188, 199);
 
             if (MouseSelect)
             {
                 _c = new Colour(255, 244, 233);
 
-                if (Layout.Size != (410d, 260d) && !(_sizeAnimation.Animating && _sizeAnimation.StartValue == 0d))
+                if (Layout.Size != _sizeAnimation.EndValue && (!_sizeAnimation.Animating || _sizeAnimation.Reversed))
                 {
-                    _sizeAnimation.Start(0d, 1d, Hande.Animator);
+                    _sizeAnimation.Reversed = false;
+                    _sizeAnimation.Reset(Hande.Animator);
                 }
             }
             else if (MouseHover)
             {
                 _c = new Colour(199, 144, 202);
-                if (Layout.Size != (410d, 260d) && !(_sizeAnimation.Animating && _sizeAnimation.StartValue == 0d))
+                if (Layout.Size != _sizeAnimation.EndValue && (!_sizeAnimation.Animating || _sizeAnimation.Reversed))
                 {
-                    _sizeAnimation.Start(0d, 1d, Hande.Animator);
+                    _sizeAnimation.Reversed = false;
+                    _sizeAnimation.Reset(Hande.Animator);
                 }
             }
             else
             {
-                if (Layout.Size != (400d, 250d) && !(_sizeAnimation.Animating && _sizeAnimation.StartValue == 1d))
+                if (Layout.Size != _sizeAnimation.StartValue && !(_sizeAnimation.Animating && _sizeAnimation.Reversed))
                 {
-                    _sizeAnimation.Start(1d, 0d, Hande.Animator);
+                    _sizeAnimation.Reversed = true;
+                    _sizeAnimation.Reset(Hande.Animator);
                 }
             }
         }
 
         protected override void OnScroll(ScrollEventArgs e)
         {
-            base.OnScroll(e);
-
             if (this[Mods.Control])
             {
                 _g._radius += e.DeltaY * 0.01;
@@ -81,7 +77,7 @@ namespace GUITest
                 return;
             }
 
-            _g._borderWidth += e.DeltaY;
+            _g.BorderWidth += e.DeltaY;
         }
 
         private class Renderer : GraphicsManager<TestElement>
@@ -94,7 +90,21 @@ namespace GUITest
             private readonly Font _f = SampleFont.GetInstance();
 
             public double _radius = 0.2;
-            public double _borderWidth = 10;
+            private double _borderWidth = 10;
+            public double BorderWidth
+            {
+                get => _borderWidth;
+                set
+                {
+                    _borderWidth = value;
+                    Size = Source.Layout.Size;
+                }
+            }
+
+            protected override void OnSizeChange(VectorEventArgs e)
+            {
+                Size = e.Value + _borderWidth;
+            }
 
             public override void OnRender(DrawManager context)
             {
@@ -103,8 +113,7 @@ namespace GUITest
                 {
                     bc = new Colour(200, 100, 97);
                 }
-                Size = Source.Size + _borderWidth;
-                
+
                 context.DrawBorderBox(Source.Bounds, Source._c, _borderWidth, bc, _radius);
 
                 TextRenderer.Colour = new ColourF(1f, 1f, 1f);
